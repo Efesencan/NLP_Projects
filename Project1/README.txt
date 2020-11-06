98d274a10e12ff7379039fe0de62e5814e3928dd
efesencan <efe.sencan@hotmail.com>
2020-11-06 18:44:27 +0300

    added pre and post titles

---
+++ Project1/README.txt
@@ -0,0 +1,7 @@
+Pretitles:
+	https://www.buyuknet.com/akademik-unvanlar-siralamasi-t47277.0.html
+	https://mfa.gov.ct.tr/wp-content/uploads/2014/02/devlet_protokol_listesi.pdf
+
+Names:
+	https://raw.githubusercontent.com/eoner/turkce_isimler/master/tr_isim_erkek.csv
+
--- Project1/ner.py
+++ Project1/ner.py
@@ -2,6 +2,7 @@
 # -*- coding: gb18030 -*-
 import re
 
+
 inputFile = open("input.txt", "r", encoding='utf-8')
 with open("name.txt", encoding='utf-8') as name:
     name_content = [line.rstrip() for line in name]
@@ -58,18 +59,19 @@ for line in inputFile:  # iterate for each line
                 for i in match:
                     print("Date:", i.strip())
     store_long_match = []
-    for month_text in month: # 12 Eylül Salı olan case
+    for month_text in month:  # 12 Eylül Salı olan case
         if month_text in line:
             my_month = month_text
             for day_text in day:
                 if day_text in line:
                     text_day = day_text
-                    long_match = re.findall(f'\d*\s*{my_month}\s*{text_day}', line)
+                    long_match = re.findall(
+                        f'\d*\s*{my_month}\s*{text_day}', line)
                     if len(long_match):
                         for i in long_match:
                             store_long_match.append(i.strip())
                             print("Date:", i.strip())
-    for month_text in month: # 12 Eylül 1935
+    for month_text in month:  # 12 Eylül 1935
         if month_text in line:
             my_month = month_text
             match = re.findall(f'\d*\s*{my_month}\s*\d*', line)
@@ -82,13 +84,30 @@ for line in inputFile:  # iterate for each line
                     if(show):
                         print("Date:", i.strip())
 
-
     # RULE for NAME
+    names = []
+    isEntered = 0
+    for pre_title in pre_titles:
+        match = re.findall(f'{pre_title}\s*([A-ZÇĞİÖŞÜ]+[a-zçğıöşü]*\s[A-ZÇĞİÖŞÜ]*[a-zçğıöşü]*\.*\s*[A-ZÇĞİÖŞÜ]+[a-zçğıöşü]*)', line)
+        if len(match):
+            isEntered = 1
+            for name in match:
+                if name not in names:
+                    print("Name:",name)
+                    names.append(name)
     
-    
+    for post_title in post_titles:
+        match = re.findall(f'([A-ZÇĞİÖŞÜ]+[a-zçğıöşü]*)\s*{post_title}', line)
+        if len(match):
+            isEntered = 1
+            for name in match:
+                print("Name:",name)
+    match = re.findall(r'leri\s',line)
+    match = re.findall(r'ları')
+    match = re.findall(r'imiz\s')
+    match = re.findall(r'ımız\s[A-ZÇĞİÖŞÜ]+[a-zçğıöşü]*\s[A-ZÇĞİÖŞÜ]*[a-zçğıöşü]*')
+    # hiçbi rütbeli kısıma girmediyse buradan isim listesi kontrol edilecek, orda da yoksa organizasyon büyük iht. ve lokasyon
+    # Efe topu tuttu. (İsim Listesi gerekecek).
 
-
-                
-
-
+            
     # location (şehri/ilçesi/beldesi/mahallesi/apartmanı/caddesi/bölge/)
